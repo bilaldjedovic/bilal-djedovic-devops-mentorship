ff3ba90966e162310de51dc8170c8cd4c0eb91b1
Bilal <bilaldjedovic@gmail.com>
2023-03-11 19:10:39 +0100

    Finished with writing scripts

---
+++ week-3/chapter-13/script01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#basic for command
+
+for test in Berlin Vienna Munich Salzburg Sarajevo
+do
+    echo The next town is $test
+done
---
+++ week-3/chapter-13/script02.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#testing the for variable after looping
+
+for test in Berlin Vienna Munich Salzburg Sarajevo
+do
+    echo "The next town is $test"
+done
+    echo "The last town we visited was $test"
+test=Sarajevo
+    echo "Now we are wisiting $test"
